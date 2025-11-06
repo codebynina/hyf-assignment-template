92137bf1340c249a9e860bc06b733400aa08d860
niña <ninamarieondevilla@gmail.com>
2025-11-06 13:35:06 +0100

    Updated homework

--- courses/foundation/intro-to-javascript/Ez_Namey.js
+++ courses/foundation/intro-to-javascript/Ez_Namey.js
@@ -1,8 +1,8 @@
 let firstWords = ['Nexa','Lumo','Astra','Verde','Orbi','Pulse','Kairo','Strive','Fluxa','Vanta'];
 let secondWords = ['Echo', 'Solvi', 'Prism','Nova','Clio','Zyra','Mira','Forge','Alto','Drift'];
 
-const randomNumber1 = Math.floor(Math.random() * 8);
-const randomNumber2 = Math.floor(Math.random() * 5);
+const randomNumber1 = Math.floor(Math.random() * firstWords.length );
+const randomNumber2 = Math.floor(Math.random() * secondWords.length);
 
 const startupName = `${firstWords[randomNumber1]}${secondWords[randomNumber2]}`;
 
