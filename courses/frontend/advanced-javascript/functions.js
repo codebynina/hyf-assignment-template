c54a6dee89c53895916ba3161fab77a651bf8e58
niña <ninamarieondevilla@gmail.com>
2026-03-17 10:56:04 +0100

    Implemented Logging functions

--- courses/frontend/advanced-javascript/functions.js
+++ courses/frontend/advanced-javascript/functions.js
@@ -21,3 +21,17 @@ const button = document.getElementById("btn");
 button.addEventListener("click", () => {
   scheduleLog(5, "Called after 5 seconds");
 });
+
+/*
+4.Create two functions and assign them to two different variables. One function logs out Earth, the other function logs out Saturn. 
+Now create a new third function that has one parameter: planetLogFunction. The only thing the third function should do is call the provided parameter function. 
+Try call the third function once with the Earth function and once with the Saturn function.*/
+
+const earthLogger = () => console.log("Earth");
+const saturnLogger = () => console.log("Saturn");
+
+const logPlanet = (planetLogFunction) => {
+  planetLogFunction();
+};
+logPlanet(earthLogger);
+logPlanet(saturnLogger);
